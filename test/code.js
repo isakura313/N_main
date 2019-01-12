b265fec951a92636a9f2ddc8760d344609a4c923
isakura313 <isakura313@gmail.com>
2019-01-13 00:21:33 +0300

    lol

--- test/code.js
+++ test/code.js
@@ -3,24 +3,23 @@ let wrap = document.querySelector(".lol");
 let wrap1 = document.querySelector(".lol1");
 let button = document.querySelector(".button");
 
+
+// здесь происходит загрузка на первый слайд
 setTimeout(function(){delayShow("wrapper_1", "delay", wrap)}, 600);
 
 
-
 window.onscroll = function(){
-let pageHeight = window.scrollY;
-    if(pageHeight=>100){
+let pagePosition = window.scrollY;
+    if(pagePositiont=>100){
         setTimeout(function(){delayShow("wrapper_2", "delay1", wrap1)}, 600);
-
     }
 }
 
 
-
-
-function delayShow(x,y,z){
-    z.classList.remove(x);
-    z.classList.add(y);
+function delayShow(existClass,addClass,e){
+    //первый аргумент - имеющийся класс, второй добавляющийся, третий - на какой элемент назначается
+    e.classList.remove(existClass);
+    e.classList.add(addClass);
 }
 
 
