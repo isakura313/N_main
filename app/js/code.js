a37ea031afb496cb71c17bee7bcc5ce8eaba346e
Puss in Boots <isakura313@gmail.com>
2019-02-12 17:05:35 +0300

    lol

--- app/js/code.js
+++ app/js/code.js
@@ -1,1 +1,25 @@
-new WOW().init();
+$(document).ready(function(){
+
+   
+    $('.row_img').magnificPopup({delegate: 'a', 
+        type: 'image',
+        gallery :{
+            enabled: true
+        },
+        removalDelay: 300,
+      
+      mainClass: 'mfp-fade'
+      });
+    $('.academic_information_first_column').magnificPopup({delegate: 'a', 
+        type: 'image',
+        gallery :{
+            enabled: true
+        },
+        removalDelay: 300,
+      
+      mainClass: 'mfp-fade'
+      });
+      
+
+    });
+   
