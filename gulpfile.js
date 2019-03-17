62fe9e7f02a782547d56c9324514454e1fabc825
isakura313 <isakura313@gmail.com>
2019-03-17 21:24:39 +0300

    lol

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,53 @@
-let gulp  = require('gulp'),
-  // uglify  = require('gulp-uglify'),
- cssnano  = require('gulp-cssnano'),
- concatCss = require('gulp-concat-css'),
- rename   = require('gulp-rename'),
- // del      = require('del'),
- // imagemin = require('gulp-imagemin'),
- // cache    = require('gulp-cache'),
- notify   = require('gulp-notify');
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	cssnano = require('gulp-cssnano'),
+	concat = require('gulp-concat-css'),
+	rename = require('gulp-rename'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	//  cache    = require('gulp-cache'),
+	notify = require('gulp-notify'),
+	filesize = require('filesize');
+	var Client = require('ftp');
+	var fs = require('fs');
+
+//добавить ftp
+
+	function csslibs() {
+		return gulp.src('alter_app/css/**/*.css') // Выбираем файл для минификации
+			// .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+			.pipe(concat('style.css'))
+			.pipe(cssnano()) // Сжимаем
+			.pipe(gulp.dest('../dist/css')); // Выгружаем в папку alter_app/css
+	}
+
+	gulp.task('clean', function() {
+		return del.sync('../dist'); // Удаляем папку ../dist перед сборкой
+	});
 
 
 
+	function img() {
+		return gulp.src('alter_app/img/**/*') // Берем все изображения из alter_app
+			.pipe(gulp.dest('../dist/img')); // Выгружаем на продакшен
+	}
 
-function css_lib(){
-	return gulp.src('app/css/*.css') // Выбираем файл для минификации
-		.pipe(cssnano()) // Сжимаем
-		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-		.pipe(concatCss("css/style.css"))
-		.pipe(gulp.dest('dist/')) // Выгружаем в папку app/css
-		.pipe(notify("vse sdelal!"));
-};
 
-gulp.task('default', css_lib);
+	gulp.task('default', function() {
+		csslibs();
+		img();
+		var buildFonts = gulp.src('alter_app/fonts/**/*') // Переносим шрифты в продакшен
+		.pipe(gulp.dest('../dist/fonts'));
+	
+		var buildJs = gulp.src('alter_app/js/**/*') // Переносим скрипты в продакшен
+		.pipe(gulp.dest('../dist/js'));
+	
+		var buildHtml = gulp.src('alter_app/*.html') // Переносим HTML в продакшен
+		.pipe(gulp.dest('../dist'));
+	
+	});
 
-// const imagemin = require('imagemin');
-// const imageminPngquant = require('imagemin-pngquant');
-// const ftp = require( 'vinyl-ftp' ) //здесь добавляется ftp загрузка
-// imagemin(['img/*.png'], 'build/images', {use: [imageminPngquant()]}).then(() => {
-//     console.log('Images optimized');
-// });
 
+	// gulp.task('clear', function (callback) {
+	// 	return cache.clearAll();
+	// })
