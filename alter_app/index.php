ee938b18d79be3d180a9955951d7c49125c47ab8
Павел Якупов <isakura313@gmail.com>
2019-04-22 20:47:25 +0300

    all

--- alter_app/index.php
+++ alter_app/index.php
@@ -1,20 +1,26 @@
 <?php
    $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
  
-   if(!empty($_POST['name']) and !empty($_POST['telephone']) and !empty($_POST['email']) 
-   and !empty($_POST['message'])){
-      $name = trim(strip_tags($_POST['name']));
-      $phone = trim(strip_tags($_POST['telephone']));
-      $mail = trim(strip_tags($_POST['email']));
-      $message = trim(strip_tags($_POST['message']));
+
+   echo $_GET['name']." ";
+
+   // if(!empty($_GET['name']) and !empty($_GET['email'] !empty($_GET['telephone'])) 
+   // and !empty($_GET['message'])){
+      $name = trim(strip_tags($_GET['name']));
+      $phone = trim(strip_tags($_GET['telephone']));
+      $mail = trim(strip_tags($_GET['email']));
+      $message = trim(strip_tags($_GET['message']));
  
-      mail('n.chizhikova25@gmail.com', 'Письмо с сайта психолога Натальи Чижиковой', 
-      'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail.'<br />
-      Его сообщение: '.$message,"Content-type:text/html;charset=windows-1251");
+      mail('isakura313@gmail.com', 'Cообщение с вебсайта nchizhikova.ru', 
+      'Кто вам пишет '.$name.'<br />Телефончик: '.$phone.'<br />Его почта '.$mail.'<br />
+      Сообщение пользователя '.$message,"Content-type:text/html;charset=utf-8");
  
       echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
       ближайшее время<Br> $back";
  
       exit;
-   } 
+   // } 
+   // else{
+   //    echo "you fuck up";
+   // }
 ?>
