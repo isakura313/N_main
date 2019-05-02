a0e9e8d5450213ca3efa9530883b4673059b7599
Павел Якупов <isakura313@gmail.com>
2019-05-02 14:13:16 +0300

    all

---
+++ alter_app/includes/footer.inc.php
@@ -0,0 +1,51 @@
+
+<footer>
+    <div class="wrap_footer_head column is-full">
+        <h3 class="wrap_footer_head_h3  has-text-centered">Форма обратной связи</h3>
+        <h5 class="wrap_footer_head_h5  has-text-centered">Здесь можно договориться о встрече или задать
+            интересующий вас вопрос</h5>
+    </div>
+    <div class="columns main_form">
+        <div class="field column is-5">
+            <form action="index.php" method="GET">
+                <div class="control has-icons-left input_first">
+                    <input name="name" class="input is-medium" type="text" placeholder="Ваше имя" maxlength="15" required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                </div>
+                <div  class="control has-icons-left input_first">
+                    <input name="email" class="input is-medium" type="text" placeholder="Ваша почта" maxlength="30" required>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                </div>
+                <div class="control has-icons-left input_first">
+                    <input  name="telephone" class="input is-medium" type="text" placeholder="Ваш телефон" maxlength="15" required>
+                    <span class="icon is-small is-left ">
+                        <i class="fas fa-mobile-alt"></i>
+                    </span>
+                </div>
+        </div>
+        <div class="column is-5 ">
+            <textarea class="textarea is-medium" name="message" id="texta" rows="6" cols="40"
+                placeholder="Тема обращения" required></textarea>
+            <input type="submit" class="button is-pulled-right" value="Отправить">
+            </form>
+        </div>
+    </div>
+    <div class="footer_low_wrapper columns">
+        <div class="text_right column is-four-fifths">
+            <p> &copy; 2018 yakupoff </p>
+            <p>All Rights Reserved</p>
+        </div>
+        <div class="social_icons column">
+            <a href="mailto:n.chizhikova25@gmail.com"> <img src="img/footer_envelope.png" alt="письмо"></a>
+            <a href="https://t-do.ru/NataliaChizhikova"> <img src="img/footer_telegram.png" alt="telegram"></a>
+            <a href="https://www.facebook.com/profile.php?id=100001813093691"> <img
+                    src="img/footer_facebook — копия.png" alt=""></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,116 @@
 var gulp = require('gulp'),
+	sass = require('gulp-sass'),
 	uglify = require('gulp-uglify'),
 	cssnano = require('gulp-cssnano'),
 	concat = require('gulp-concat-css'),
 	rename = require('gulp-rename'),
 	del = require('del'),
 	imagemin = require('gulp-imagemin'),
-	cache  = require('gulp-cache'),
+	cache = require('gulp-cache'),
 	notify = require('gulp-notify'),
 	filesize = require('filesize');
-	var Client = require('ftp');
-	var fs = require('fs');
+var Client = require('ftp');
+var fs = require('fs');
 
-//сделать перенос php
-function php(){
-	return gulp.src("alter_app/**/*.php")
-	.pipe(gulp.dest("../dist"));
+sass.compiler = require('node-sass');
+
+const cssFiles = [
+	'alter_app/css_libs/**/*.css',
+	'alter_app/css/**/*.css '
+]
+
+
+function php() {
+	return gulp.src("alter_app/include/**/*.php")
+		.pipe(gulp.dest("../dist"));
 
 }
-gulp.task('php',function(){
-	php();
+// gulp.task('sass:watch', function () {
+// 	gulp.watch('alter_app/sass/**/*.sass', ['sass']);
+//   });
+
+
+gulp.task('php', php); 
+gulp.task('sass', sw); 
+gulp.task('csslibs', csslibs );
+gulp.task(' watchFiles',  watchFiles);
+
+function watchFiles(){
+	gulp.watch('alter_app/sass/**/*.sass', sw);
+}
+
+function watchcss(){
+	gulp.watch('alter_app/css/style.css', csslibs);
+}
+
+gulp.task('watch', watchFiles);
+gulp.task('watch1', watchcss);
+
+function watchFiles(){
+	gulp.watch('alter_app/sass/**/*.sass', sw);
+}
+
+function sw(){
+	return gulp.src('alter_app/sass/**/*.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('alter_app/css'));
+		
+}
+gulp.task('multi', function(){
+	var sass = gulp.src('alter_app/sass/**/*.sass')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('alter_app/css'));
+	var csslibs = gulp.src(cssFiles)
+	// .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+	.pipe(concat('style.css'))
+	.pipe(cssnano()) // Сжимаем
+	.pipe(gulp.dest('../dist/css')) // Выгружаем в папку alter_app/css
+
+	return merge(sass, csslibs);
 })
 
-	function csslibs() {
-		return gulp.src('alter_app/css/**/*.css') // Выбираем файл для минификации
-			// .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-			.pipe(concat('style.css'))
-			.pipe(cssnano()) // Сжимаем
-			.pipe(gulp.dest('../dist/css')); // Выгружаем в папку alter_app/css
-	}
 
-	gulp.task('clean', function() {
-		return del.sync('../dist'); // Удаляем папку ../dist перед сборкой
-	});
+ 
 
+function csslibs() {
+	return gulp.src(cssFiles)
+		
+		// .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+		.pipe(concat('style.css'))
+		.pipe(cssnano()) // Сжимаем
+		.pipe(gulp.dest('alter_app/css')) // Выгружаем в папку alter_app/css
+}
 
-
-	function img() {
-		return gulp.src('alter_app/img/**/*') // Берем все изображения из alter_app
-			.pipe(gulp.dest('../dist/img')); // Выгружаем на продакшен
-	}
+gulp.task('clean', function () {
+	return del.sync('../dist'); // Удаляем папку ../dist перед сборкой
+});
 
 
-	gulp.task('default', function() {
-		php();
-		csslibs();
-		img();
-		var buildFonts = gulp.src('alter_app/fonts/**/*') // Переносим шрифты в продакшен
+
+function img() {
+	return gulp.src('alter_app/img/**/*') // Берем все изображения из alter_app
+		.pipe(gulp.dest('../dist/img')); // Выгружаем на продакшен
+}
+
+
+gulp.task('default', function () {
+	sass();
+	// sass_change();
+	php();
+	csslibs();
+	img();
+	var buildFonts = gulp.src('alter_app/fonts/**/*') // Переносим шрифты в продакшен
 		.pipe(gulp.dest('../dist/fonts'));
-	
-		var buildJs = gulp.src('alter_app/js/**/*') // Переносим скрипты в продакшен
+
+	var buildJs = gulp.src('alter_app/js/**/*') // Переносим скрипты в продакшен
 		.pipe(gulp.dest('../dist/js'));
-	
-		var buildHtml = gulp.src('alter_app/*.html') // Переносим HTML в продакшен
+
+	var buildHtml = gulp.src('alter_app/*.html') // Переносим HTML в продакшен
 		.pipe(gulp.dest('../dist'));
-	
-	});
+
+});
 
 
-	gulp.task('clear', function (callback) {
-		return cache.clearAll();
-	})
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
